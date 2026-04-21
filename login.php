d25d70698a33492a9d7fe20c08e74aacae91dc2d
kevins006 <kschmid2006@gmail.com>
2026-04-21 16:53:55 -0400

    latest db

--- login.php
+++ login.php
@@ -2,6 +2,8 @@
 session_start();
 require "common.php";
 
+$mode = isset($_GET["mode"]) ? $_GET["mode"] : "login";
+
 // Handle login
 if (isset($_POST["login"])) {
     $username = $_POST["username"];
@@ -34,6 +36,7 @@ if (isset($_POST["login"])) {
 if (isset($_POST["register"])) {
     $password = $_POST["password"];
     $confirm = $_POST["confirm_password"];
+    $mode = "register";
 
     if ($password !== $confirm) {
         $reg_error = "Passwords do not match.";
@@ -48,6 +51,7 @@ if (isset($_POST["register"])) {
         );
         if ($result === true) {
             $reg_success = "Account created! You can now log in.";
+            $mode = "login";
         } else {
             $reg_error = "Registration failed: " . $result;
         }
@@ -56,33 +60,38 @@ if (isset($_POST["register"])) {
 ?>
 <html>
 <body>
-<h2>Login</h2>
-<?php if (isset($error)) echo "<p style='color:red'>$error</p>"; ?>
-<form method="post" action="login.php">
-    Username: <input type="text" name="username"><br>
-    Password: <input type="password" name="password"><br>
-    Role:
-    <select name="role">
-        <option value="customer">Customer</option>
-        <option value="employee">Employee</option>
-    </select><br>
-    <input type="submit" name="login" value="Login">
-</form>
+<?php if ($mode === "login"): ?>
+    <h2>Login</h2>
+    <?php if (isset($error)) echo "<p style='color:red'>$error</p>"; ?>
+    <form method="post" action="login.php">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        Role:
+        <select name="role">
+            <option value="customer">Customer</option>
+            <option value="employee">Employee</option>
+        </select><br>
+        <input type="submit" name="login" value="Login">
+    </form>
+    <br>
+    <a href="login.php?mode=register">New customer? Create an account</a>
 
-<hr>
-
-<h2>New Customer? Register Here</h2>
-<?php if (isset($reg_error)) echo "<p style='color:red'>$reg_error</p>"; ?>
-<?php if (isset($reg_success)) echo "<p style='color:green'>$reg_success</p>"; ?>
-<form method="post" action="login.php">
-    Username: <input type="text" name="username"><br>
-    First Name: <input type="text" name="first_name"><br>
-    Last Name: <input type="text" name="last_name"><br>
-    Email: <input type="text" name="email"><br>
-    Shipping Address: <input type="text" name="shipping_address"><br>
-    Password: <input type="password" name="password"><br>
-    Confirm Password: <input type="password" name="confirm_password"><br>
-    <input type="submit" name="register" value="Register">
-</form>
+<?php else: ?>
+    <h2>Create Account</h2>
+    <?php if (isset($reg_error)) echo "<p style='color:red'>$reg_error</p>"; ?>
+    <?php if (isset($reg_success)) echo "<p style='color:green'>$reg_success</p>"; ?>
+    <form method="post" action="login.php?mode=register">
+        Username: <input type="text" name="username"><br>
+        First Name: <input type="text" name="first_name"><br>
+        Last Name: <input type="text" name="last_name"><br>
+        Email: <input type="text" name="email"><br>
+        Shipping Address: <input type="text" name="shipping_address"><br>
+        Password: <input type="password" name="password"><br>
+        Confirm Password: <input type="password" name="confirm_password"><br>
+        <input type="submit" name="register" value="Register">
+    </form>
+    <br>
+    <a href="login.php">Already have an account? Login here</a>
+<?php endif; ?>
 </body>
 </html>
